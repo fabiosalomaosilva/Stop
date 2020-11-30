537536d8267abf8c0218381f1f7c81bd7d639c65
Fábio Salomão Silva <fabio@arquivarnet.com.br>
2020-11-30 12:01:22 -0500

    Adicionar arquivos de projeto.

---
+++ Stop/Services/BlazorTimer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Timers;
+
+namespace Stop.Services
+{
+    public class BlazorTimer
+    {
+        private Timer _timer;
+
+        public void SetTimer(double interval)
+        {
+            _timer = new Timer(interval);
+            _timer.Elapsed += NotifyTimerElapsed;
+            _timer.Enabled = true;
+        }
+
+        public void EndTimer()
+        {
+            _timer.Stop();
+            _timer.Close();
+        }
+
+        public event Action OnElapsed;
+
+        private void NotifyTimerElapsed(Object source, ElapsedEventArgs e)
+        {
+            OnElapsed?.Invoke();
+            _timer.Dispose();
+        }
+    }
+}
